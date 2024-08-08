50c6b4e80e2f280f6092feb81ce67399833e9b0a
Nicolas Brandoni <nico.brandoni98@gmail.com>
2024-08-08 18:19:12 -0300

    asd

--- part3/index.js
+++ part3/index.js
@@ -1,27 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-let notes = []
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
-  })
-  console.log(note)
-  response.json(note)
-})
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    return note.id === id;
+  });
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end()
+  }
+});
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-const PORT = 3001
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
