a9c674d4a3c034220aee6f5ab2195103433a16f1
Nicolas Brandoni <nico.brandoni98@gmail.com>
2024-08-08 19:32:06 -0300

    alfin

--- part3/api/index.js
+++ part3/api/index.js
@@ -41,6 +41,10 @@ app.get("/api/notes", (request, response) => {
 });
 
 const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
+
