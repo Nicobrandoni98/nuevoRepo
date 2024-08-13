99bc654bd93d334f3992e3936ac288ab21b98bed
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-08-13 18:49:09 -0300

    vamos de nuevo2

--- part3/api/index.js
+++ part3/api/index.js
@@ -1,34 +1,56 @@
 const express = require("express");
 const app = express();
 
-let notes = [
+app.use(express.json());
+
+const cors = require('cors');
+
+
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+
+app.use(cors())
+app.use(bodyParser.json());
+
+morgan.token('body', (request) => {
+  return request.body ? JSON.stringify(request.body) : '-'
+})
+
+const customFormat = ':method :url :status :response-time ms - :res[content-length] - Body: :body'
+
+app.use(morgan(customFormat))
+
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
+  const person = persons.find((person) => person.id === id);
 
-  if (note) {
-    response.json(note);
+  if (person) {
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
 });
 
@@ -36,15 +58,62 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
+
+app.get("/api/info", (request, response) => {
+  const info = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toISOString()}</p>
+  `;
+  response.send(info);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const {name, number} = request.body;
+
+  if (!name || !number)
+    return response
+      .status(400)
+      .json({ error: "El nombre y el numero son requeridos" });
+
+  const nameExists = persons.some((person) => person.name === name);
+  if (nameExists)
+    return response.status(400).json({ error: "el nombre debe ser unico" });
+
+  const ids = persons.map((person) => person.id);
+  const maxId = Math.max(...ids);
+
+  const newPerson = {
+    id: maxId + 1,
+    name: name,
+    number: number
+  };
+
+  persons = [...persons, newPerson];
+
+  response.json(newPerson);
+});
+
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
-
